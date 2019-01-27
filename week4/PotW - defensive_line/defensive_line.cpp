3c6ca41fd3895b5c884a6b875ee113dd16c8c9ba
Claude Hähni <chaehni@student.ethz.ch>
2019-01-27 22:16:03 +0100

    new defensive line

--- week4/PotW - defensive_line/defensive_line.cpp
+++ week4/PotW - defensive_line/defensive_line.cpp
@@ -3,74 +3,76 @@
 
 using namespace std;
 
-void run(){
+void run()
+{
+    int N, M, K;
+    cin >> N >> M >> K;
 
-    int n; cin >> n; // #defenders
-    int m; cin >> m; // #attackers
-    int k; cin >> k; // attacker strength
-
-    vector<int> defence(n);
-    vector<int> cum_def(n);
-
-    cin >> defence[0];
-    cum_def[0] = defence[0];
-    for (int i = 1; i < n; i++){
-        cin >> defence[i];
-        cum_def[i] = cum_def[i-1] + defence[i];
+    vector<int> sum(N);
+    int cum = 0;
+    for (int i = 0; i < N; i++)
+    {
+        int temp;
+        cin >> temp;
+        cum += temp;
+        sum[i] = cum;
     }
-    
-    //sliding window
-    int i = 0;
-    int j = 0;
-    vector<int> walls(n,0); // ith element saves the length of the defence-wall of value k that ends in i, if no wall ends there value is 0
-    int cnt = 0;
 
-    while (i < n){
-        // get defence value uf interval [j, i]
-        int value = cum_def[i] - cum_def[j] + defence[j];
-        if (value == k){
-            walls[i] = i-j+1;
-            if (i == 0){
-            }
-            i++; j++;
-        } else if (value < k){
-            i++;
-        } else if (value > k) {
-            if (i == j){
-                i++;
-            } else {
-                j++;
-            }
+    // ends[i] contains the length of a defensive line of strength K that ends in i
+    vector<int> ends(N, 0);
+    int l = 0;
+    int r = 0;
+
+    while (r < N)
+    {
+
+        int value = r == 0 ? sum[0] : sum[r] - sum[l - 1];
+        if (value == K)
+        {
+            ends[r] = r - l + 1;
+            r++;
+            l++;
         }
-    }
-    
-    vector<vector<int>> table(m+1, vector<int>(n)); // table[i][j] saves the biggest value when first m attacker attack first j defenders
 
-    for (int j = 0; j < n; j++){
-        table[0][j] = 0;   // 0 attackers cannot attack any value
-    }
-    for (int i = 1; i < m+1; i++){
-        table[i][0] = walls[0]; // can the first defender be attacked? (v0 == k)
-    }
+        if (value < K)
+            r++;
 
-    for (int i = 1; i < m+1; i++){
-        for(int j = 1; j < n; j++){
-            table[i][j] = max(table[i][j-1], walls[j]+table[i-1][j-walls[j]]); // either don't pick the interval ending in j or pick it plus the maximum from j-interval_length
+        if (value > K)
+        {
+            if (l < r)
+                l++;
+            else
+                r++;
         }
     }
 
-    if (table[m-1][n-1] != table[m][n-1]){
-        cout << table[m][n-1] << "\n";
-    } else {
-        cout << "fail" << "\n";
+    // DP table
+    vector<vector<int>> table(M, vector<int>(N));
+
+    for (int i = 0; i < M; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            int o1 = j > 0 ? table[i][j - 1] : 0;                                         // take the value from left
+            int o2 = ends[j];                                                             //only take the line ending in j (initialization for one attacker)
+            int o3 = i > 0 && j - ends[j] >= 0 ? ends[j] + table[i - 1][j - ends[j]] : 0; // take the line ending in j plus the max we can get with i-1 attackers up to
+                                                                                          //the start of the line we took
+
+            table[i][j] = max(max(o1, o2), o3);
+        }
     }
-        
+
+    if (M >= 2 && table[M - 1][N - 1] == table[M - 2][N - 1]) // value has to increase for every additional attacker
+        cout << "fail" << endl;
+    else
+        cout << table[M - 1][N - 1] << endl;
 }
 
-int main(){
-    std::ios_base::sync_with_stdio(false);
-    int tests; cin >> tests;
-    while (tests--){
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    int n;
+    cin >> n;
+    while (n--)
         run();
-    }
 }
