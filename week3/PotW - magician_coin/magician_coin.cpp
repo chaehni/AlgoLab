19aeb502bee61924416d12f6b068d0bd7e8b8f35
Claude Hähni <chaehni@student.ethz.ch>
2019-01-28 19:15:26 +0100

    magician and the coin

--- week3/PotW - magician_coin/magician_coin.cpp
+++ week3/PotW - magician_coin/magician_coin.cpp
@@ -1,57 +1,52 @@
 #include <iostream>
-#include <vector>
 #include <iomanip>
+#include <vector>
 
 using namespace std;
 
-vector<vector<double>> max_prob;
-vector<double> probs;
+double play(vector<double> &p, int wealth, int goal, int day, int N, vector<vector<double>> &memo)
+{
+    // if we played N times we need at least 'goal' money
+    if (day == N && wealth < goal)
+        return 0.0;
 
-double rec(int k, int m, int depth, int n){
+    if (wealth >= goal)
+        return 1.0;
 
-    //recursion termination: after the last game n, we need at least m money
-    if (k >= m)
-        return 1;
-    if (depth == n && k < m)
-        return 0;
-    
-    if (max_prob[k][depth] != -1){
-        return max_prob[k][depth];
-    } else {
-        double max = 0;
-        double p = probs[depth];
-        for (int i = 0; i <= k; i++){
-            double temp = p * rec(k+i, m, depth+1, n) + (1-p) * rec(k-i, m, depth+1, n);
-            if(temp > max)
-                max = temp;
-        }
-        max_prob[k][depth] = max;
-        return max;
-    }
+    // check memo
+    if (memo[day][wealth] != -1)
+        return memo[day][wealth];
+
+    // else try to bet every amount and record max
+    double best = 0;
+    for (int i = 0; i <= wealth; i++)
+        best = max(best, p[day] * play(p, wealth + i, goal, day + 1, N, memo) +
+                             (1 - p[day]) * play(p, wealth - i, goal, day + 1, N, memo));
+
+    memo[day][wealth] = best;
+    return best;
 }
 
-void run(){
+void run()
+{
+    int N, K, M;
+    cin >> N >> K >> M;
 
-    int n; cin >> n;
-    int k; cin >> k;
-    int m; cin >> m;
-    probs = vector<double>(n);
+    // read probabilities
+    vector<double> p(N);
+    for (int i = 0; i < N; i++)
+        cin >> p[i];
 
-    for (int i = 0; i < n; i++){
-        double temp; cin >> temp;
-        probs[i] = temp;
-    }
-    
-    max_prob = vector<vector<double>> (m+1, vector<double>(n, -1));
+    vector<vector<double>> memo(N, vector<double>(M, -1));
+    cout << play(p, K, M, 0, N, memo) << endl;
+}
 
+int main()
+{
+    ios_base::sync_with_stdio(false);
     cout << fixed << setprecision(5);
-    cout << rec(k,m,0,n) << "\n";
-}
-
-int main(){
-    std::ios_base::sync_with_stdio(false);
-    int tests; cin >> tests;
-    while (tests--){
+    int n;
+    cin >> n;
+    while (n--)
         run();
-    }
 }
