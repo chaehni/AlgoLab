438db2c580a8d40019d54180617c89609a8e2f35
Claude Hähni <chaehni@student.ethz.ch>
2020-01-07 21:28:11 +0100

    evolution

--- 2019/week11/evolution/src/algorithm.cpp
+++ 2019/week11/evolution/src/algorithm.cpp
@@ -2,10 +2,11 @@
 #include <vector>
 #include <unordered_map>
 #include <stack>
+#include <algorithm>
 
 using namespace std;
 
-struct speciesy
+struct species
 {
     int index;
     int age;
@@ -16,21 +17,24 @@ void run()
 {
     int n, q;
     cin >> n >> q;
-    unordered_map<string, specie> m;           // maps from name to species
-    vector<vector<species>> tree(n);           // save species in the tree with their name and age (index is implicit)
-    typedef pair<int, int> query;              // a query is defined as the index of a specie and an age
-    vector<vector<pair<int, int>>> queries(n); // records all the queries for a given species by the index of the query and the age to query
-    vector<pair<int, string>> answers(q);      // holds the answers to the queries (index of query and name)
+    unordered_map<string, species> m;  // maps from name to species
+    vector<vector<species>> tree(n);   // models the tree. Every species has a vector indices of its children
+    typedef pair<int, int> query;      // a query is defined as the index of the query and an age
+    vector<vector<query>> queries(n);  // records all the queries for a given species by the index of the query and the age to query
+    vector<pair<int, string>> answers; // holds the answers to the queries (index of query and name)
+    answers.reserve(n);
 
     int max_age = 0;
     string luca;
+    int cnt = 0;
+
     // read all species
     for (int i = 0; i < n; i++)
     {
         string name;
         int age;
         cin >> name >> age;
-        m[name] = make_pair(i, age);
+        m[name] = species{cnt++, age, name};
         if (age > max_age)
         {
             max_age = age;
@@ -43,9 +47,7 @@ void run()
     {
         string child, parent;
         cin >> child >> parent;
-        int child_age = m[child].second;
-        int parent_id = m[parent].first;
-        tree[parent_id].emplace_back(child, child_age);
+        tree[m[parent].index].push_back(m[child]);
     }
 
     for (int i = 0; i < q; i++)
@@ -53,39 +55,67 @@ void run()
         string s;
         int a;
         cin >> s >> a;
-        queries[m[s].first].emplace_back(i, a);
+        queries[m[s].index].emplace_back(i, a);
     }
 
     // run DFS from LUCA and for every discovered species answer all queries
-    stack<specie> s;
+    stack<species> s;
     unordered_map<int, bool> visited;
-    vector<specie> leg;
-    specie l = m[luca];
+    vector<species> leg;
+    species l = m[luca];
     s.push(l);
-    visited[l.first] == true;
-    leg.push_back(l);
 
     while (!s.empty())
     {
-        specie curr = s.top();
+        species curr = s.top();
 
         // if we came back to this species we have handled it and all its children - > pop it
-        if (visited[curr.first])
+        if (visited[curr.index])
         {
             s.pop();
             leg.pop_back();
             continue;
         }
 
+        visited[curr.index] = true;
+
         // if we have not seen this species, add it to the leg and run all it's queries
         leg.push_back(curr);
 
-        for (auto q : queries[curr.first])
+        for (auto q : queries[curr.index])
         {
-            int index = q.first;
-            int age = q.second;
+            int q_index = q.first;
+            int q_age = q.second;
+
+            // run a binary search on leg to find oldest ancestor of curr not older than age
+            int l = 0;
+            int r = leg.size() - 1;
+            while (l != r)
+            {
+                int middle = (l + r) / 2;
+                if (leg[middle].age > q_age)
+                    l = middle + 1;
+                else
+                    r = middle;
+            }
+            answers.emplace_back(q_index, leg[l].name);
         }
+
+        // add all children to the stack
+        for (species sp : tree[curr.index])
+            s.push(sp);
     }
+
+    // sort answers array and produce output
+    sort(answers.begin(), answers.end());
+
+    for (int i = 0; i < q; i++)
+    {
+        cout << answers[i].second;
+        if (i != q - 1)
+            cout << " ";
+    }
+    cout << "\n";
 }
 
 int main(int argc, char const *argv[])
